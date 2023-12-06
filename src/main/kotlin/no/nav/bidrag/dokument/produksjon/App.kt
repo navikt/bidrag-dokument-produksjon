d27768129255be13922138059d73969af37e4d98
Ugur Cenar <cenar93@gmail.com>
2023-12-06 21:16:31 +0100

    Opprett notat

--- src/main/kotlin/no/nav/bidrag/dokument/produksjon/App.kt
+++ src/main/kotlin/no/nav/bidrag/dokument/produksjon/App.kt
@@ -1,9 +1,13 @@
 package no.nav.bidrag.dokument.produksjon
 
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(
+    exclude = [SecurityAutoConfiguration::class, ManagementWebSecurityAutoConfiguration::class]
+)
 class App
 
 fun main(args: Array<String>) {
