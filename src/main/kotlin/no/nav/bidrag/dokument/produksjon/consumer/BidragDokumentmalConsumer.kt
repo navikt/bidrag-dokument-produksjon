cfdaecf6e8128f9ae29450d154fb15da20b02b90
Ugur Cenar <cenar93@gmail.com>
2024-04-15 10:18:28 +0200

    Legg til logging

--- src/main/kotlin/no/nav/bidrag/dokument/produksjon/consumer/BidragDokumentmalConsumer.kt
+++ src/main/kotlin/no/nav/bidrag/dokument/produksjon/consumer/BidragDokumentmalConsumer.kt
@@ -1,11 +1,14 @@
 package no.nav.bidrag.dokument.produksjon.consumer
 
+import io.github.oshai.kotlinlogging.KotlinLogging
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.http.HttpHeaders
 import org.springframework.stereotype.Component
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.client.postForEntity
+
+private val log = KotlinLogging.logger {}
 
 @Component
 class BidragDokumentmalConsumer(
@@ -24,6 +27,7 @@ class BidragDokumentmalConsumer(
                     .build()
             restTemplate.postForEntity<String>(malUrl, payload).body!!
         } catch (e: Exception) {
+            log.error(e) { "Det skjedde en feil ved henting av dokumentmal fra url $url" }
             null
         }
     }
