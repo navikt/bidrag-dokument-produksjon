15d0203b29ce61fb5bb9ae5b9d6db0709acdd537
Ugur Cenar <cenar93@gmail.com>
2024-04-18 08:47:46 +0200

    Notat vis med mellomrom

--- bidrag-dokumentmal/app/components/Notat.tsx
+++ bidrag-dokumentmal/app/components/Notat.tsx
@@ -1,5 +1,6 @@
 import { Notat as NotatData } from "~/types/Api";
 import Datadisplay from "~/components/Datadisplay";
+import xss from "xss";
 
 type NotatProps = {
   data: NotatData;
@@ -15,12 +16,19 @@ export default function Notat({ data }: NotatProps) {
     >
       <Datadisplay
         label={"Begrunnelse (med i vedtaket)"}
-        value={data.medIVedtaket}
+        value={<PurifiedHtml text={data.medIVedtaket} />}
       />
       <Datadisplay
         label={"Begrunnelse (kun for intern notat)"}
-        value={data.intern}
+        value={<PurifiedHtml text={data.intern} />}
       />
     </div>
   );
+}
+
+function PurifiedHtml({ text }: { text?: string }) {
+  if (text == null || text === "") return null;
+  const cleanText = xss(text.replace(/(?:\r\n|\r|\n)/g, "<br />"));
+
+  return <span dangerouslySetInnerHTML={{ __html: cleanText }} />;
 }
