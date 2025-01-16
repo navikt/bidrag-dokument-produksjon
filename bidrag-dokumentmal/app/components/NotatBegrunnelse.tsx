bf2a25f8bf185e5567014b89fd36796ad6982961
Ugur Alpay Cenar <cenar93@gmail.com>
2025-01-16 13:35:11 +0100

    Støtte for mer ekspressiv begrunnelsefelt

--- bidrag-dokumentmal/app/components/NotatBegrunnelse.tsx
+++ bidrag-dokumentmal/app/components/NotatBegrunnelse.tsx
@@ -11,7 +11,7 @@ export default function NotatBegrunnelse({ data }: NotatProps) {
       style={{
         maxWidth: "35rem",
       }}
-      className={"mt-2"}
+      className={"mt-2 break-all"}
     >
       <DataViewTable
         data={
@@ -36,7 +36,104 @@ export default function NotatBegrunnelse({ data }: NotatProps) {
 
 function PurifiedHtml({ text }: { text?: string }) {
   if (text == null || text === "") return null;
-  const cleanText = xss(text.replace(/(?:\r\n|\r|\n)/g, "<br />"));
+  const cleanText = xss(text.replace(/(?:\r\n|\r|\n)/g, "<br />"), {
+    whiteList: {
+      ...getDefaultWhiteList(),
+      em: ["style"],
+    },
+  });
 
   return <span dangerouslySetInnerHTML={{ __html: cleanText }} />;
+}
+function getDefaultWhiteList() {
+  return {
+    a: ["target", "href", "title"],
+    abbr: ["title"],
+    address: [],
+    area: ["shape", "coords", "href", "alt"],
+    article: [],
+    aside: [],
+    audio: [
+      "autoplay",
+      "controls",
+      "crossorigin",
+      "loop",
+      "muted",
+      "preload",
+      "src",
+    ],
+    b: [],
+    bdi: ["dir"],
+    bdo: ["dir"],
+    big: [],
+    blockquote: ["cite"],
+    br: [],
+    caption: [],
+    center: [],
+    cite: [],
+    code: [],
+    col: ["align", "valign", "span", "width"],
+    colgroup: ["align", "valign", "span", "width"],
+    dd: [],
+    del: ["datetime"],
+    details: ["open"],
+    div: [],
+    dl: [],
+    dt: [],
+    em: [],
+    figcaption: [],
+    figure: [],
+    font: ["color", "size", "face"],
+    footer: [],
+    h1: [],
+    h2: [],
+    h3: [],
+    h4: [],
+    h5: [],
+    h6: [],
+    header: [],
+    hr: [],
+    i: [],
+    img: ["src", "alt", "title", "width", "height", "loading"],
+    ins: ["datetime"],
+    kbd: [],
+    li: [],
+    mark: [],
+    nav: [],
+    ol: [],
+    p: [],
+    pre: [],
+    s: [],
+    section: [],
+    small: [],
+    span: [],
+    sub: [],
+    summary: [],
+    sup: [],
+    strong: [],
+    strike: [],
+    table: ["width", "border", "align", "valign"],
+    tbody: ["align", "valign"],
+    td: ["width", "rowspan", "colspan", "align", "valign"],
+    tfoot: ["align", "valign"],
+    th: ["width", "rowspan", "colspan", "align", "valign"],
+    thead: ["align", "valign"],
+    tr: ["rowspan", "align", "valign"],
+    tt: [],
+    u: [],
+    ul: [],
+    video: [
+      "autoplay",
+      "controls",
+      "crossorigin",
+      "loop",
+      "muted",
+      "playsinline",
+      "poster",
+      "preload",
+      "src",
+      "height",
+      "width",
+    ],
+  };
 }
