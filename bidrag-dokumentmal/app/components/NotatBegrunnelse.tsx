3c95a5974ec827fdaf5c71299004826a43d39a82
Ugur Alpay Cenar <cenar93@gmail.com>
2025-01-17 10:40:17 +0100

    Støtte for mer ekspressiv begrunnelsefelt (#160)
    * Støtte for mer ekspressiv begrunnelsefelt
    
    * Støtte for mer ekspressiv begrunnelsefelt

--- bidrag-dokumentmal/app/components/NotatBegrunnelse.tsx
+++ bidrag-dokumentmal/app/components/NotatBegrunnelse.tsx
@@ -8,10 +8,11 @@ type NotatProps = {
 export default function NotatBegrunnelse({ data }: NotatProps) {
   return (
     <div
+      className={"mt-2"}
       style={{
         maxWidth: "35rem",
+        overflowWrap: "anywhere",
       }}
-      className={"mt-2"}
     >
       <DataViewTable
         data={
@@ -36,7 +37,115 @@ export default function NotatBegrunnelse({ data }: NotatProps) {
 
 function PurifiedHtml({ text }: { text?: string }) {
   if (text == null || text === "") return null;
-  const cleanText = xss(text.replace(/(?:\r\n|\r|\n)/g, "<br />"));
+  const cleanText = xss(
+    text
+      .replace(/(?:\r\n|\r|\n)/g, "<br />")
+      .replaceAll("<p><br/></p>", "<br/>"),
+    {
+      whiteList: {
+        ...getDefaultWhiteList(),
+        em: ["style"],
+        p: ["style"],
+      },
+    },
+  );
 
-  return <span dangerouslySetInnerHTML={{ __html: cleanText }} />;
+  return (
+    <span
+      className={"begrunnelse"}
+      dangerouslySetInnerHTML={{ __html: cleanText }}
+    />
+  );
+}
+function getDefaultWhiteList() {
+  return {
+    a: ["target", "href", "title"],
+    abbr: ["title"],
+    address: [],
+    area: ["shape", "coords", "href", "alt"],
+    article: [],
+    aside: [],
+    audio: [
+      "autoplay",
+      "controls",
+      "crossorigin",
+      "loop",
+      "muted",
+      "preload",
+      "src",
+    ],
+    b: [],
+    bdi: ["dir"],
+    bdo: ["dir"],
+    big: [],
+    blockquote: ["cite"],
+    br: [],
+    caption: [],
+    center: [],
+    cite: [],
+    code: [],
+    col: ["align", "valign", "span", "width"],
+    colgroup: ["align", "valign", "span", "width"],
+    dd: [],
+    del: ["datetime"],
+    details: ["open"],
+    div: [],
+    dl: [],
+    dt: [],
+    em: [],
+    figcaption: [],
+    figure: [],
+    font: ["color", "size", "face"],
+    footer: [],
+    h1: [],
+    h2: [],
+    h3: [],
+    h4: [],
+    h5: [],
+    h6: [],
+    header: [],
+    hr: [],
+    i: [],
+    img: ["src", "alt", "title", "width", "height", "loading"],
+    ins: ["datetime"],
+    kbd: [],
+    li: [],
+    mark: [],
+    nav: [],
+    ol: [],
+    p: [],
+    pre: [],
+    s: [],
+    section: [],
+    small: [],
+    span: [],
+    sub: [],
+    summary: [],
+    sup: [],
+    strong: [],
+    strike: [],
+    table: ["width", "border", "align", "valign"],
+    tbody: ["align", "valign"],
+    td: ["width", "rowspan", "colspan", "align", "valign"],
+    tfoot: ["align", "valign"],
+    th: ["width", "rowspan", "colspan", "align", "valign"],
+    thead: ["align", "valign"],
+    tr: ["rowspan", "align", "valign"],
+    tt: [],
+    u: [],
+    ul: [],
+    video: [
+      "autoplay",
+      "controls",
+      "crossorigin",
+      "loop",
+      "muted",
+      "playsinline",
+      "poster",
+      "preload",
+      "src",
+      "height",
+      "width",
+    ],
+  };
 }
