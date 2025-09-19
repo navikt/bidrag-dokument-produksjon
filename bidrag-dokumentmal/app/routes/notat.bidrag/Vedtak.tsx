e0113dc9cd3ad36ee91641917a375e9c9bb916b0
Ugur Alpay Cenar <cenar93@gmail.com>
2025-09-19 14:51:06 +0200

    Ny mal forside orkestrert vedtak

--- bidrag-dokumentmal/app/routes/notat.bidrag/Vedtak.tsx
+++ bidrag-dokumentmal/app/routes/notat.bidrag/Vedtak.tsx
@@ -322,7 +322,7 @@ export function VedtakEndeligTable({
               width: visningForSaksbehandler ? "120px" : "30%",
             },
             {
-              name: visningForSaksbehandler ? "Type" : "Vedtak/Justering",
+              name: visningForSaksbehandler ? "Type" : "Vedtak/justering",
               width: visningForSaksbehandler ? "100px" : "40%",
             },
             { name: "Beløp", width: visningForSaksbehandler ? "120px" : "30%" },
