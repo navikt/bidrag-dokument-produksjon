70831f973e4c974e446e0ed0b4f2ea41fb612b37
Ugur Alpay Cenar <cenar93@gmail.com>
2025-10-13 19:55:39 +0200

    Visning av innkreving

--- bidrag-dokumentmal/app/components/notat_felles/components/Virkningstidspunkt.tsx
+++ bidrag-dokumentmal/app/components/notat_felles/components/Virkningstidspunkt.tsx
@@ -11,84 +11,180 @@ import Beregningsperiode from "~/routes/notat.bidrag/Beregningsperiode";
 
 export default function Virkningstidspunkt() {
   const { data } = useNotatFelles();
-  const virkningstidspunkt = data.virkningstidspunkt;
+  const virkningstidspunkt = data.virkningstidspunktV2;
   const behandling = data.behandling;
   return (
     <div className={"virkningstidspunkt"}>
       <h2>Virkningstidspunkt</h2>
-      <div>
-        <div className={"flex flex-row justify-between w-[500px]"}>
-          <DataViewTable
-            labelColWidth={"70px"}
-            data={[
-              {
-                label: "Søknadstype",
-                value: capitalizeFirstLetter(behandling.søknadstype),
-              },
-              {
-                label: "Søknad fra",
-                value: søktAvTilVisningsnavn(behandling.søktAv),
-              },
-              ...[
-                behandling.avslag
-                  ? {
-                      label: "Avslag",
-                      value: behandling.avslagVisningsnavn,
-                    }
-                  : {
-                      label: "Årsak",
-                      value: virkningstidspunkt.årsakVisningsnavn,
-                    },
-              ],
-            ]}
-          />
-          <DataViewTable
-            labelColWidth={"100px"}
-            data={[
-              {
-                label: "Mottatt dato",
-                value: dateToDDMMYYYY(behandling.mottattDato as string),
-              },
-              {
-                label: "Søkt fra dato",
-                value: dateToDDMMYYYY(behandling.søktFraDato as string),
-              },
-            ]}
-          />
-        </div>
+      {virkningstidspunkt.erLikForAlle ? (
+        <VirkningstidspunktFelles />
+      ) : (
+        <VirkningstidspunktPerBarn />
+      )}
+    </div>
+  );
+}
+
+function VirkningstidspunktPerBarn() {
+  const { data } = useNotatFelles();
+  const virkningstidspunkt = data.virkningstidspunkt;
+  const behandling = data.behandling;
+  return (
+    <div>
+      <div className={"flex flex-row justify-between w-[500px]"}>
         <DataViewTable
-          labelColWidth={"100px"}
-          className={"mt-4"}
+          labelColWidth={"70px"}
           data={[
             {
-              label: "Virkningstidspunkt",
-              value: dateToDDMMYYYY(behandling.virkningstidspunkt),
+              label: "Søknadstype",
+              value: capitalizeFirstLetter(behandling.søknadstype),
+            },
+            {
+              label: "Søknad fra",
+              value: søktAvTilVisningsnavn(behandling.søktAv),
             },
           ]}
         />
-        {data.roller
-          .filter((d) => d.opphørsdato != null)
-          .map((rolle) => (
-            <DataViewTable
-              key={rolle.ident}
-              labelColWidth={"100px"}
-              data={[
-                {
-                  label: "Opphørsdato",
-                  value: dateToDDMMYYYY(rolle.opphørsdato),
-                },
-              ]}
-            />
-          ))}
-        <Beregningsperiode />
-        <NotatBegrunnelse data={virkningstidspunkt.begrunnelse} />
-        {data.stønadstype == Stonadstype.BIDRAG18AAR && (
-          <NotatBegrunnelse
-            label={"Vurdering av skolegang"}
-            data={virkningstidspunkt.begrunnelseVurderingAvSkolegang}
-          />
-        )}
+        <DataViewTable
+          labelColWidth={"100px"}
+          className={"mb-4"}
+          data={[
+            {
+              label: "Mottatt dato",
+              value: dateToDDMMYYYY(behandling.mottattDato as string),
+            },
+            {
+              label: "Søkt fra dato",
+              value: dateToDDMMYYYY(behandling.søktFraDato as string),
+            },
+          ]}
+        />
       </div>
+      <div className={"flex flex-col gap-2"}>
+        {data.virkningstidspunktV2.barn.map((barn) => {
+          return (
+            <div>
+              <DataViewTable
+                data={[
+                  {
+                    label: "Barn i saken",
+                    labelBold: true,
+                    value: barn.rolle.navn,
+                  },
+                ]}
+              />
+              <DataViewTable
+                labelColWidth={"100px"}
+                data={[
+                  {
+                    label: "Virkningstidspunkt",
+                    value: dateToDDMMYYYY(barn.virkningstidspunkt),
+                  },
+                ]}
+              />
+              <DataViewTable
+                key={barn.rolle.ident}
+                labelColWidth={"100px"}
+                data={[
+                  {
+                    label: "Opphørsdato",
+                    value: dateToDDMMYYYY(barn.opphørsdato as string),
+                  },
+                ]}
+              />
+              <Beregningsperiode virkningstidspunkt={barn} />
+              <NotatBegrunnelse data={virkningstidspunkt.begrunnelse} />
+              {data.stønadstype == Stonadstype.BIDRAG18AAR && (
+                <NotatBegrunnelse
+                  label={"Vurdering av skolegang"}
+                  data={virkningstidspunkt.begrunnelseVurderingAvSkolegang}
+                />
+              )}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+function VirkningstidspunktFelles() {
+  const { data } = useNotatFelles();
+  const virkningstidspunkt = data.virkningstidspunkt;
+  const behandling = data.behandling;
+  return (
+    <div>
+      <div className={"flex flex-row justify-between w-[500px]"}>
+        <DataViewTable
+          labelColWidth={"70px"}
+          data={[
+            {
+              label: "Søknadstype",
+              value: capitalizeFirstLetter(behandling.søknadstype),
+            },
+            {
+              label: "Søknad fra",
+              value: søktAvTilVisningsnavn(behandling.søktAv),
+            },
+            ...[
+              behandling.avslag
+                ? {
+                    label: "Avslag",
+                    value: behandling.avslagVisningsnavn,
+                  }
+                : {
+                    label: "Årsak",
+                    value: virkningstidspunkt.årsakVisningsnavn,
+                  },
+            ],
+          ]}
+        />
+        <DataViewTable
+          labelColWidth={"100px"}
+          data={[
+            {
+              label: "Mottatt dato",
+              value: dateToDDMMYYYY(behandling.mottattDato as string),
+            },
+            {
+              label: "Søkt fra dato",
+              value: dateToDDMMYYYY(behandling.søktFraDato as string),
+            },
+          ]}
+        />
+      </div>
+      <DataViewTable
+        labelColWidth={"100px"}
+        className={"mt-4"}
+        data={[
+          {
+            label: "Virkningstidspunkt",
+            value: dateToDDMMYYYY(behandling.virkningstidspunkt),
+          },
+        ]}
+      />
+      {data.roller
+        .filter((d) => d.opphørsdato != null)
+        .map((rolle) => (
+          <DataViewTable
+            key={rolle.ident}
+            labelColWidth={"100px"}
+            data={[
+              {
+                label: "Opphørsdato",
+                value: dateToDDMMYYYY(rolle.opphørsdato),
+              },
+            ]}
+          />
+        ))}
+      <Beregningsperiode virkningstidspunkt={virkningstidspunkt} />
+      <NotatBegrunnelse data={virkningstidspunkt.begrunnelse} />
+      {data.stønadstype == Stonadstype.BIDRAG18AAR && (
+        <NotatBegrunnelse
+          label={"Vurdering av skolegang"}
+          data={virkningstidspunkt.begrunnelseVurderingAvSkolegang}
+        />
+      )}
     </div>
   );
 }
--- bidrag-dokumentmal/app/routes/notat.bidrag/Beregningsperiode.tsx
+++ bidrag-dokumentmal/app/routes/notat.bidrag/Beregningsperiode.tsx
@@ -1,23 +1,27 @@
 import { dateToDDMMYYYY, deductDays } from "~/utils/date-utils";
 import { useNotatFelles } from "~/components/notat_felles/NotatContext";
 import { DataViewTable } from "~/components/DataViewTable";
-import { BeregnTil, NotatMalType, Vedtakstype } from "~/types/Api";
+import {
+  BeregnTil,
+  NotatMalType,
+  NotatVirkningstidspunktBarnDto,
+} from "~/types/Api";
 
-export default function Beregningsperiode() {
+export default function Beregningsperiode({
+  virkningstidspunkt,
+}: {
+  virkningstidspunkt: NotatVirkningstidspunktBarnDto;
+}) {
   const { data, type } = useNotatFelles();
-  const virkningstidspunkt = data.virkningstidspunkt;
   const skalViseBeregningsperiode =
     type == NotatMalType.BIDRAG && data.erOrkestrertVedtak;
   if (!skalViseBeregningsperiode) return null;
-  const erKlage = data.behandling.vedtakstype === Vedtakstype.KLAGE;
   function beregningsperiodeVisningsnavn() {
     switch (virkningstidspunkt.beregnTil) {
       case BeregnTil.ETTERFOLGENDEMANUELLVEDTAK:
         return `Til etterfølgende vedtak med virkningstidspunkt ${dateToDDMMYYYY(virkningstidspunkt.etterfølgendeVedtakVirkningstidspunkt as string)}`;
       case BeregnTil.INNEVAeRENDEMANED:
         return "Ut inneværende måned";
-      case BeregnTil.OMGJORT_VEDTAK_VEDTAKSTIDSPUNKT:
-        return `Ut måneden ${erKlage ? "påklagd" : "omgjort"} vedtak ble fattet`;
       case BeregnTil.OPPRINNELIG_VEDTAKSTIDSPUNKT:
         return `Ut måneden opprinnelig vedtak ble fattet`;
     }
